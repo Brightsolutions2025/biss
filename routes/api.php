4d52e066ae6a51c06100630fb44601e4a1454899
Brightsolutions2025 <brightsolutionsystem@gmail.com>
2025-08-05 10:19:39 +0000

    API

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\LarkWebhookController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::post('/webhook/lark', [LarkWebhookController::class, 'handle']);
+Route::post('/webhooks/lark', [LarkWebhookController::class, 'handle']);
 //Route::post('/webhooks/lark', [LarkWebhookController::class, 'receive']);
