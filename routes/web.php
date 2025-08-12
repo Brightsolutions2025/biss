735db33e09c27195f76e8fa0da66aac49ed9800b
Brightsolutions2025 <brightsolutionsystem@gmail.com>
2025-08-12 09:27:11 +0000

    Corrected Routes

--- routes/web.php
+++ routes/web.php
@@ -207,14 +207,12 @@ Route::middleware('auth', EnsureUserIsAdmin::class, EnsureUserHasCompany::class)
     Route::resource('users', UserController::class);
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::post('/tickets/{ticket}/approve', [TicketController::class, 'approve'])->name('tickets.approve');
-    Route::post('/tickets/{ticket}/assign', [TicketController::class, 'assignPerson'])->name('tickets.assign');
-    Route::get('/tickets/{ticket}/assign', [TicketAssignmentController::class, 'edit']);
-    Route::post('/tickets/{ticket}/assign', [TicketAssignmentController::class, 'update'])->name('tickets.assign.update');
-    Route::post('/tickets/{ticket}/assign', [TicketController::class, 'assign']);
-});
-
-
+//Route::middleware(['auth'])->group(function () {
+//    Route::post('/tickets/{ticket}/approve', [TicketController::class, 'approve'])->name('tickets.approve');
+//    Route::post('/tickets/{ticket}/assign', [TicketController::class, 'assignPerson'])->name('tickets.assign');
+//    Route::get('/tickets/{ticket}/assign', [TicketAssignmentController::class, 'edit']);
+//    Route::post('/tickets/{ticket}/assign', [TicketAssignmentController::class, 'update'])->name('tickets.assign.update');
+//    Route::post('/tickets/{ticket}/assign', [TicketController::class, 'assign']);
+//});
 
 require __DIR__ . '/auth.php';
